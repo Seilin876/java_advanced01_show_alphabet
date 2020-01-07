c1d9f6059965c4772264bc230d6f092c3059f42b
shanglin10 <a0926753805@gmail.com>
2020-01-08 01:40:45 +0800

    1090108update

---
+++ src/main/java/com/example/advancedhw01/MyClass.java
@@ -0,0 +1,13 @@
+package com.example.advancedhw01;
+
+public class MyClass {
+    public static void main (String[] args)
+    {for(char i=65;i<123;i++){
+        System.out.print(i);
+    if(i==90){
+    i+=6;
+        System.out.println();}
+
+    }
+    }
+    }
